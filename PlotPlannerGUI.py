851ea8d81353f9ac918ffc2a7b9f72ed07efbdec
oh-yes-0-fps <foryt.bowan@gmail.com>
2022-11-13 02:23:36 -0500

    early gui stuff

--- PlotPlannerGUI.py
+++ PlotPlannerGUI.py
@@ -1,10 +1,28 @@
 import PySimpleGUI as gui
 
+template = {
+            "array_names": [],
+            "trace_names": [],
+            "sources": [],
+            "blacklisted_types": []
+        }
+
+group_data = {}
+
+groups = list(group_data.keys())
+
+
+
+def rename_dict_keys(dict, old_key, new_key):
+    dict[new_key] = dict.pop(old_key)
+
+
+
 # Define the window's contents
 layout = [
-            [gui.Text("Plot Planner")],
-            [gui.Listbox(values=('Plot 1', 'Plot 2', 'Plot 3'), size=(30, 3), key='-LIST-')],
-            [gui.Button('Edit', enable_events=True, key='-EDIT-'), gui.Button('Add', enable_events=True, key='-ADD-'), gui.Button('Delete', enable_events=True, key='-DELETE-')]
+            [gui.Text("Plot Planner"), gui.Button('Plot Settings', enable_events=True, key='plot_settings')],
+            [gui.Listbox(values=groups, size=(30, 3), key='-LIST-', auto_size_text=True)],
+            [gui.Button('Edit', enable_events=True, key='-EDIT-'), gui.Button('Add', enable_events=True, key='-ADD-'), gui.Button('Delete', enable_events=True, key='-DELETE-'), gui.Button('Rename', enable_events=True, key='-RENAME-')],
          ]
 
 # Create the window
@@ -13,13 +31,35 @@ window = gui.Window('Plot Planner', layout=layout, finalize=True, resizable=True
 window['-LIST-'].expand(expand_x=True, expand_y=True)
 
 
+def find(str, list):
+    for i in range(len(list)):
+        if str == list[i]:
+            return i
+    return 0
+
+def update_list():
+    # new_groups = []
+    # for i in group_data:
+    #     new_groups.append(i)
+    # groups = new_groups
+    groups = list(group_data.keys())
+    window['-LIST-'].update(values=groups)
+
 # Display and interact with the Window using an Event Loop
 while True:
+    update_list()
     event, values = window.read()
     # See if user wants to quit or window was closed
     if event == gui.WIN_CLOSED or event == 'Cancel':
         break
-    try:
-        print('You selected ', values['-LIST-'][0])
-    except:
-        pass
+    if event is None:
+        break
+    if event == '-DELETE-':
+        del group_data[values['-LIST-'][0]]
+    if event == '-ADD-':
+        group_data[gui.popup_get_text('Rename Group', 'Enter new name', default_text='New Group')] = template.copy()
+    if event == '-RENAME-':
+        new = gui.popup_get_text('Rename Group', 'Enter new name', default_text=values['-LIST-'][0])
+        rename_dict_keys(group_data, values['-LIST-'][0], new)
+    # if event == '-EDIT-':
+
--- test.py
+++ test.py
@@ -1,18 +1,34 @@
-from types import NoneType
 import datalogHandler as dlh
+import json
+
 
 SMALL_FILE = 'FRC_20221022_150128_NYROC_Q17.wpilog'
 
 LARGE_FILE = 'FRC_20221012_000713.wpilog'
 
-import time
-import datetime
+
 
 if __name__ == '__main__':
+
+    jdata = {
+        'names': [],
+        'sources': [],
+        'canids': [],
+        'arraynames': [],
+    }
+
     log = dlh.DatalogHandler(SMALL_FILE)
     for i in log:
-        print(i)
-
-
+        i:dlh.trace_structure
+        jdata['names'].append(i.name)
+        jdata['sources'].extend(i.metadata.get('SOURCE', []))
+        jdata['canids'].extend(i.metadata.get('CANID', []))
+        jdata['arraynames'].extend(i.metadata.get('ARRAY', []))
+    jdata['sources'] = list(set(jdata['sources']))
+    jdata['canids'] = list(set(jdata['canids']))
+    jdata['arraynames'] = list(set(jdata['arraynames']))
+    file_name = SMALL_FILE.removesuffix('.wpilog')
+    with open(f'.\\resources\\cached\\{file_name}.json', 'w') as f:
+        json.dump(jdata, f)
     pass
 
